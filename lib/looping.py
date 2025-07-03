50642f91a0f902ac97c449a18849a9f7b86506e2
Justin <justinagui988@gmail.com>
2025-07-03 09:53:38 +0300

    implement looping functions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python3
-
+# happy_new_year: countdown from 10 to 1 then prints "Happy New Year!"
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
+# square_integers: square each element in the list and returns new list
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num * num for num in int_list]
 
+# fizzbuzz: print numbers 1 to 100 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
